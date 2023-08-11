98499f2fa2c393602b16424c784d0fb0705faf39
Crys <mariano.christopher123@gmail.com>
2023-08-11 21:40:56 +0800

    edit contract

--- contracts/PokemonDollar.sol
+++ contracts/PokemonDollar.sol
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title PokemonDollar - A decentralized stablecoin
@@ -10,6 +13,35 @@ pragma solidity ^0.8.18;
  *
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system
  */
-contract PokemonDollar {
-    constructor() {}
+contract PokemonDollar is ERC20Burnable, Ownable {
+    error PokemonDollar_MustBeMoreThanZero();
+    error PokemonDollar_BurnAmountExceedsBalance();
+    error PokemonDollar_NotZeroAddress();
+
+    constructor() ERC20("PokemonDollar", "PD") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert PokemonDollar_MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert PokemonDollar_BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert PokemonDollar_NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert PokemonDollar_MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
 }
