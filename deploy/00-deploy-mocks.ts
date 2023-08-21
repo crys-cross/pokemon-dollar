b3e99addfa7402270877683bbfe713e01db90090
Crys <mariano.christopher123@gmail.com>
2023-08-21 13:55:24 +0800

    edited  mocks for testing

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,6 +1,6 @@
 // reserved for mocks
 // deploy mockv3agreggator(ETHUSDPricefeed, BTCUSDPriceFeed)
-// deploy erc20mock(eth, btc)
+// deploy erc20mock(eth, btc)(deployed only on localhost) TODO:
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { networkConfig } from "../helper-hardhat-config";
@@ -17,7 +17,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const args1: any[] = [
     networkConfig[network.config.chainId!]["DECIMALS"],
     networkConfig[network.config.chainId!]["ETH_USD_PRICE"],
-    // address _pdAddress
   ];
   const ethUsdPriceFeed = await deploy("MockV3Aggregator", {
     from: deployer,
@@ -26,22 +25,9 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     // waitConfirmations: waitBlockConfirmations,
   });
 
-  const args2: any[] = [
-    networkConfig[network.config.chainId!]["DECIMALS"],
-    networkConfig[network.config.chainId!]["ETH_USD_PRICE"],
-    // address _pdAddress
-  ];
-  const wethMock = await deploy("ERC20Mock", {
-    from: deployer,
-    log: true,
-    args: args2,
-    // waitConfirmations: waitBlockConfirmations,
-  });
-
   const args3: any[] = [
     networkConfig[network.config.chainId!]["DECIMALS"],
-    networkConfig[network.config.chainId!]["ETH_USD_PRICE"],
-    // address _pdAddress
+    networkConfig[network.config.chainId!]["BTC_USD_PRICE"],
   ];
   const btcUsdPriceFeed = await deploy("MockV3Aggregator", {
     from: deployer,
@@ -50,17 +36,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     // waitConfirmations: waitBlockConfirmations,
   });
 
-  const args4: any[] = [
-    networkConfig[network.config.chainId!]["DECIMALS"],
-    networkConfig[network.config.chainId!]["ETH_USD_PRICE"],
-    // address _pdAddress
-  ];
-  const wbtcMock = await deploy("ERC20Mock", {
-    from: deployer,
-    log: true,
-    args: args4,
-    // waitConfirmations: waitBlockConfirmations,
-  });
+  // deploy ERC20Mocksonly on localhost
+  if (chainId === 31337) {
+    const args2: any[] = ["WETH", "WETH", deployer, 1000e8];
+    const wethMock = await deploy("ERC20Mock", {
+      from: deployer,
+      log: true,
+      args: args2,
+      // waitConfirmations: waitBlockConfirmations,
+    });
+
+    const args4: any[] = ["WBTC", "WBTC", deployer, 1000e8];
+    const wbtcMock = await deploy("ERC20Mock", {
+      from: deployer,
+      log: true,
+      args: args4,
+      // waitConfirmations: waitBlockConfirmations,
+    });
+  }
 
   //    // Verify the deployment
   //    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
