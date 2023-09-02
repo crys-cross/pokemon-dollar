41f56bc0696c3e6399dbcc855e98eb1c8420bf50
Crys <mariano.christopher123@gmail.com>
2023-09-02 21:28:40 +0800

    edited unit tests

--- test/unit/dSCEngine.test.ts
+++ test/unit/dSCEngine.test.ts
@@ -6,7 +6,33 @@ import { deployments, ethers, network } from "hardhat";
 import { subtask } from "hardhat/config";
 import { developmentChains, networkConfig } from "../../helper-hardhat-config";
 import { DSCEngine } from "../../typechain-types";
+import { beforeEach } from "mocha";
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("DSCEngine Unit Test", () => {});
+  : describe("DSCEngine Unit Test", () => {
+      let dSCEngine: DSCEngine;
+
+      beforeEach(async () => {});
+
+      describe("constructor", () => {
+        it("", async () => {});
+      });
+
+      describe("price", () => {
+        it("", async () => {});
+      });
+
+      // test below need own setup
+      describe("deposit collateral", () => {
+        it("", async () => {});
+      });
+
+      describe("deposit cillateral and mint", () => {
+        it("", async () => {});
+      });
+
+      describe("mint PD", () => {
+        it("", async () => {});
+      });
+    });
