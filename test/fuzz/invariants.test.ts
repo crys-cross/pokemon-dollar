1723a173de951c83e40ab02dc2ba7867cf4eeef8
Crys <mariano.christopher123@gmail.com>
2023-09-22 20:07:29 +0800

    edited unit tests and contract

--- test/fuzz/invariants.test.ts
+++ test/fuzz/invariants.test.ts
@@ -1,2 +1,7 @@
 // Have our invariant aka properties hold true for all the time
 // What are our invariants?
+// 1. The total supply of PD should be less than the total value of collateral
+// 2. Getter view functions should never revert <- evergreen invariant
+// chainlink test link - https://www.youtube.com/live/dDr7glOjtvI?si=MiQZpfwlTRdafvqb
+// https://github.com/andrejrakic/hardhat-testing
+// TODO: hardhat fuzztest
